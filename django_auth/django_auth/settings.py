5725eae73b2bf50c3d2e12e3e3192862c4381692
prerantej <prerantejkvr@gmail.com>
2024-07-20 20:18:32 +0530

    password change functionality is added through mail using the PasswordResetView PasswordResetDoneView PasswordResetConfirmView PasswordResetCompleteView

--- django_auth/django_auth/settings.py
+++ django_auth/django_auth/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,10 +123,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'  # For Gmail
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = env('EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'prearntejkvr@gmail.com'
+EMAIL_HOST_PASSWORD = 'yjqq fhjt ormn txhp'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
